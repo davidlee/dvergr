15104ffd851738a277adc013f8066e2fb51b9643
David Lee <dav@davlee.com>
2023-11-12 00:47:46 +1100

    rewrite it to use bevy_ecs_tilemap from example code

--- src/main.rs
+++ src/main.rs
@@ -1,166 +1,149 @@
-//! Shows how to render simple primitive shapes with a single color.
-
-// use bevy::window::WindowPlugin;
 use bevy::{
-    core::FrameCount,
-    // diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
-    sprite::Anchor,
     window::{PresentMode, WindowTheme},
 };
 
-// #[derive(Clone, Debug)]
-// struct Position {
-//     x: u16,
-//     y: u16,
-//     z: u16,
-// }
+use bevy_ecs_tilemap::prelude::*;
+// mod helpers;
 
-struct Rect {
-    width: f32,
-    height: f32,
-}
-
-struct TileConfig {
-    width: f32,
-    height: f32,
-    spacing: f32,
-}
-
-impl TileConfig {
-    const fn square(n: f32, sp: f32) -> Self {
-        Self {
-            width: n,
-            height: n,
-            spacing: sp,
-        }
-    }
-
-    fn w(&self) -> f32 {
-        self.width + self.spacing
-    }
-
-    fn h(&self) -> f32 {
-        self.height + self.spacing
-    }
-
-    fn x_offset(&self, col: u16) -> f32 {
-        let k: f32 = col as f32;
-        self.width * k + self.spacing * k
-    }
-
-    fn y_offset(&self, row: u16) -> f32 {
-        let k: f32 = row as f32;
-        self.height * k + self.spacing * k
-    }
-
-    fn size(&self) -> Vec2 {
-        Vec2::new(self.width, self.height)
-    }
-}
-
-#[derive(Debug)]
-struct TileSet {
-    x_count: u16,
-    y_count: u16,
+struct Resolution {
     x: f32,
     y: f32,
 }
 
-// CONSTANTS
-const TILE_CONFIG: TileConfig = TileConfig::square(16.0, 5.0);
-
-impl TileSet {
-    fn initial() -> TileSet {
-        let x_count = (WINDOW_CONFIG.width / TILE_CONFIG.w() - 0.0) as u16;
-        let y_count = (WINDOW_CONFIG.height / TILE_CONFIG.h() - 0.0) as u16;
-
-        let width: f32 =
-            x_count as f32 * TILE_CONFIG.width + (x_count + 2) as f32 * TILE_CONFIG.spacing;
-
-        let height =
-            y_count as f32 * TILE_CONFIG.width + (y_count + 2) as f32 * TILE_CONFIG.spacing;
-
-        TileSet {
-            x_count,
-            y_count,
-            x: (TILE_CONFIG.width - width) / 2.0,
-            y: (TILE_CONFIG.height - height) / 2.0,
-        }
-    }
-}
-
-const WINDOW_CONFIG: Rect = Rect {
-    width: 800.0,
-    height: 600.0,
+const DEFAULT_RES: Resolution = Resolution {
+    x: 1024.0,
+    y: 768.0,
 };
-
-// #[derive(Debug)]
-// struct TileCount {
-//     horizontal: u16,
-//     vertical: u16,
-// }
-
-// struct Player;
 
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
+        .add_plugins((DefaultPlugins
+            .set(WindowPlugin {
                 primary_window: Some(Window {
                     title: "One day I will be a roguelike".into(),
-                    resolution: (WINDOW_CONFIG.width, WINDOW_CONFIG.height).into(),
+                    resolution: (DEFAULT_RES.x, DEFAULT_RES.y).into(),
                     present_mode: PresentMode::AutoVsync,
                     // Tells wasm to resize the window according to the available canvas
                     fit_canvas_to_parent: true,
                     // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
                     prevent_default_event_handling: false,
                     window_theme: Some(WindowTheme::Dark),
-                    enabled_buttons: bevy::window::EnabledButtons {
-                        maximize: false,
-                        ..Default::default()
-                    },
-                    // This will spawn an invisible window
-                    // The window will be made visible in the make_visible() system after 3 frames.
-                    // This is useful when you want to avoid the white window that shows up before the GPU is ready to render the app.
-                    visible: false,
                     ..default()
                 }),
                 ..default()
-            }),
-            // LogDiagnosticsPlugin::default(),
-            // FrameTimeDiagnosticsPlugin,
-        ))
-        .add_systems(Startup, setup)
-        .add_systems(Update, (tick, make_visible, bevy::window::close_on_esc))
+            })
+            .set(
+                ImagePlugin::default_nearest(),
+                // LogDiagnosticsPlugin::default(),
+                // FrameTimeDiagnosticsPlugin,
+            ),))
+        .add_plugins(TilemapPlugin)
+        .add_systems(Startup, startup)
+        .add_systems(Update, (tick, bevy::window::close_on_esc))
+        // .add_systems(Update, helpers::camera::movement)
+        .add_systems(Update, swap_texture_or_hide)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn get_tilemap_size(resolution: &Resolution, tile_size: &TilemapTileSize) -> TilemapSize {
+    let x: u32 = (resolution.x / tile_size.x) as u32;
+    let y: u32 = (resolution.y / tile_size.y) as u32;
+    TilemapSize { x, y }
+}
+
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    #[cfg(all(not(feature = "atlas"), feature = "render"))] array_texture_loader: Res<
+        ArrayTextureLoader,
+    >,
+) {
     commands.spawn(Camera2dBundle::default());
 
-    let tileset = TileSet::initial();
-    println!("TILESET {:?}", tileset);
+    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
 
-    let cols: Vec<u16> = (0..tileset.x_count).collect();
-    let rows: Vec<u16> = (0..tileset.y_count).collect();
+    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
 
-    for col in &cols {
-        for row in &rows {
-            commands.spawn(SpriteBundle {
-                sprite: Sprite {
-                    color: Color::rgb(0.45, 0.25, 0.75),
-                    custom_size: Some(TILE_CONFIG.size()),
-                    anchor: Anchor::TopLeft,
-                    ..default()
-                },
-                transform: Transform::from_translation(Vec3::new(
-                    tileset.x + TILE_CONFIG.x_offset(*col),
-                    tileset.y + TILE_CONFIG.y_offset(*row),
-                    0.,
-                )),
-                ..default()
-            });
+    let map_size = get_tilemap_size(&DEFAULT_RES, &tile_size);
+
+    // Create a tilemap entity a little early.
+    // We want this entity early because we need to tell each tile which tilemap entity
+    // it is associated with. This is done with the TilemapId component on each tile.
+    // Eventually, we will insert the `TilemapBundle` bundle on the entity, which
+    // will contain various necessary components, such as `TileStorage`.
+    let tilemap_entity = commands.spawn_empty().id();
+
+    // To begin creating the map we will need a `TileStorage` component.
+    // This component is a grid of tile entities and is used to help keep track of individual
+    // tiles in the world. If you have multiple layers of tiles you would have a tilemap entity
+    // per layer, each with their own `TileStorage` component.
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    // Spawn the elements of the tilemap.
+    // Alternatively, you can use helpers::filling::fill_tilemap.
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::default();
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
+
+    // Add atlas to array texture loader so it's preprocessed before we need to use it.
+    // Only used when the atlas feature is off and we are using array textures.
+    // #[cfg(all(not(feature = "atlas"), feature = "render"))]
+    // {
+    //     array_texture_loader.add(TilemapArrayTexture {
+    //         texture: TilemapTexture::Single(asset_server.load("tiles.png")),
+    //         tile_size,
+    //         ..Default::default()
+    //     });
+    // }
+}
+
+fn swap_texture_or_hide(
+    asset_server: Res<AssetServer>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut TilemapTexture, &mut Visibility)>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        let texture_a = TilemapTexture::Single(asset_server.load("tiles.png"));
+        let texture_b = TilemapTexture::Single(asset_server.load("tiles2.png"));
+        for (mut tilemap_tex, _) in &mut query {
+            if *tilemap_tex == texture_a {
+                *tilemap_tex = texture_b.clone();
+            } else {
+                *tilemap_tex = texture_a.clone();
+            }
+        }
+    }
+    if keyboard_input.just_pressed(KeyCode::H) {
+        for (_, mut visibility) in &mut query {
+            *visibility = match *visibility {
+                Visibility::Inherited | Visibility::Visible => Visibility::Hidden,
+                Visibility::Hidden => Visibility::Visible,
+            };
         }
     }
 }
@@ -172,14 +155,4 @@ fn setup(mut commands: Commands) {
 
 fn tick(_commands: Commands) {
     // println!("tick");
-}
-
-fn make_visible(mut window: Query<&mut Window>, frames: Res<FrameCount>) {
-    // The delay may be different for your app or system.
-    if frames.0 == 3 {
-        // At this point the gpu is ready to show the app so we can make the window visible.
-        // Alternatively, you could toggle the visibility in Startup.
-        // It will work, but it will have one white frame before it starts rendering
-        window.single_mut().visible = true;
-    }
 }
