d3570db0cd284083ffd7c88a3037389776f36c03
David Lee <dav@davlee.com>
2023-11-30 23:03:41 +1100

    add some silly 2d

--- src/graphics/playground.rs
+++ src/graphics/playground.rs
@@ -2,6 +2,7 @@
 use crate::graphics::typical::*;
 use crate::typical::*;
 use bevy::prelude::*;
+use std::f32::consts::TAU;
 
 // use super::tilemap;
 
@@ -17,18 +18,20 @@ pub fn draw_weird_lines(
         if let Position::Point(pos) = locus.position {
             let a = (clock.current_frame() % 120) as f32 / 120.0;
 
-            // // Draw a box around the player
+            let rot = (clock.current_frame() % 180) as f32 / 180.0 * TAU;
+            warn!("{:?}", rot);
+            // Draw a box around the player
             let origin = tile_map.translate(pos);
             let size = Vec2::new(TILE_SIZE_W * 3.0, TILE_SIZE_H * 3.0);
             gizmos.rect_2d(origin, 0.0, size, Color::ORANGE_RED.with_a(a));
+            gizmos.rect_2d(origin, rot, size, Color::ORANGE_RED.with_a(0.25));
 
-            // draw a line to somewhere
-            //
-            // gizmos.line_2d(
-            //     tile_map.translate(pos),
-            //     Vec2::new(0., 0.),
-            //     Color::RED.with_a(a),
-            // );
+            // draw a circle too
+            gizmos.circle_2d(
+                origin,
+                36.0 + (a * TILE_SIZE_W * 24.0),
+                Color::GOLD.with_a(1.0 - a),
+            );
         }
     }
 }
