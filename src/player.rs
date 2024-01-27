cd5e2c35640d9915d23ae478d5e28278c7243f7a
David Lee <dav@davlee.com>
2024-01-28 09:44:45 +1100

    fix: spawn 3d Player & camera as child of map

--- src/player.rs
+++ src/player.rs
@@ -49,6 +49,7 @@ pub(crate) fn spawn(
     mut commands: Commands,
     mut board: ResMut<Board>,
     mut ev: EventReader<SpawnPlayerEvent>,
+    map_query: Query<(Entity, &MapMarker)>,
 ) {
     let position = ev.read().next().unwrap().0;
 
@@ -63,66 +64,67 @@ pub(crate) fn spawn(
         ..default()
     };
 
-    //
-    let player_id = commands
-        .spawn((
-            player_bundle,
-            PlayerAvatar,
-            SpatialBundle {
-                transform: Transform::from_xyz(position.x as f32, position.y as f32, 0.),
-                ..default()
-            },
-        ))
-        .with_children(|player| {
-            player
-                .spawn((
-                    SpotLightBundle {
-                        spot_light: SpotLight {
-                            intensity: 950.,
-                            range: 120.,
-                            shadows_enabled: true,
-                            color: Color::rgba_linear(0.8, 0.3, 0.05, 1.0),
-                            outer_angle: 2.5,
-                            inner_angle: 0.2,
-                            ..default()
-                        },
-                        transform: Transform::from_xyz(0., 0., 0.25)
-                            .looking_at(Vec3::new(0., 0., 0.), Vec3::new(0., 0., 0.)),
-                        ..default()
-                    },
-                    TorchMarker,
-                ))
-                .with_children(|torch| {
-                    torch.spawn((TorchSecondaryLightMarker, SpatialBundle::default()));
-                });
-            player.spawn((
-                CameraMarker,
-                Camera3dBundle {
-                    projection: Projection::Perspective(PerspectiveProjection {
-                        fov: FOV,
-                        ..default()
-                    }),
-                    transform: Transform::from_xyz(0., 0., CAMERA3D_Z_POS)
-                        .looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
-                    camera_3d: Camera3d {
-                        clear_color: ClearColorConfig::Custom(Color::BLACK),
-                        screen_space_specular_transmission_steps: 3,
-                        screen_space_specular_transmission_quality:
-                            bevy::core_pipeline::core_3d::ScreenSpaceTransmissionQuality::Ultra,
-                        ..default()
-                    },
-                    tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::TonyMcMapface,
-                    color_grading: ColorGrading {
-                        exposure: 0.5,
-                        gamma: 1.4,
-                        pre_saturation: 0.8,
-                        post_saturation: 0.6,
-                    },
+    let (map_entity, _) = map_query.single();
+    commands.entity(map_entity).with_children(|inside_map| {
+        let player_id = inside_map
+            .spawn((
+                player_bundle,
+                PlayerAvatar,
+                SpatialBundle {
+                    transform: Transform::from_xyz(position.x as f32, position.y as f32, 0.),
                     ..default()
                 },
-            ));
-        })
-        .id();
-
-    board.creature_store.insert(player_id, position);
+            ))
+            .with_children(|player| {
+                player
+                    .spawn((
+                        SpotLightBundle {
+                            spot_light: SpotLight {
+                                intensity: 950.,
+                                range: 120.,
+                                shadows_enabled: true,
+                                color: Color::rgba_linear(0.8, 0.3, 0.05, 1.0),
+                                outer_angle: 2.5,
+                                inner_angle: 0.2,
+                                ..default()
+                            },
+                            transform: Transform::from_xyz(0., 0., 0.25)
+                                .looking_at(Vec3::new(0., 0., 0.), Vec3::new(0., 0., 0.)),
+                            ..default()
+                        },
+                        TorchMarker,
+                    ))
+                    .with_children(|torch| {
+                        torch.spawn((TorchSecondaryLightMarker, SpatialBundle::default()));
+                    });
+                player.spawn((
+                    CameraMarker,
+                    Camera3dBundle {
+                        projection: Projection::Perspective(PerspectiveProjection {
+                            fov: FOV,
+                            ..default()
+                        }),
+                        transform: Transform::from_xyz(0., 0., CAMERA3D_Z_POS)
+                            .looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
+                        camera_3d: Camera3d {
+                            clear_color: ClearColorConfig::Custom(Color::BLACK),
+                            screen_space_specular_transmission_steps: 3,
+                            screen_space_specular_transmission_quality:
+                                bevy::core_pipeline::core_3d::ScreenSpaceTransmissionQuality::Ultra,
+                            ..default()
+                        },
+                        tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::TonyMcMapface,
+                        color_grading: ColorGrading {
+                            exposure: 0.5,
+                            gamma: 1.4,
+                            pre_saturation: 0.8,
+                            post_saturation: 0.6,
+                        },
+                        ..default()
+                    },
+                ));
+            })
+            .id();
+        board.creature_store.insert(player_id, position);
+    });
 }
