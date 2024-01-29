a1ff8694d33199f9b7e9785f099484af51e40988
David Lee <dav@davlee.com>
2024-01-29 17:21:14 +1100

    feat: eh, let's keep track of entities for them cubes

--- src/goblin.rs
+++ src/goblin.rs
@@ -4,6 +4,8 @@ use crate::typical::{graphics::GoblinSpritesheet, *};
 #[derive(Event, Debug)]
 pub(crate) struct SpawnGoblinEvent(pub IVec3);
 
+// https://bevy-cheatbook.github.io/cookbook/cursor2world.html
+
 pub(crate) fn spawn_goblins(
     map_query: Query<Entity, With<MapMarker>>,
     cell_query: Query<Entity, &Cell>,
@@ -14,6 +16,9 @@ pub(crate) fn spawn_goblins(
 ) {
     for SpawnGoblinEvent(position) in ev_gobs.read() {
         let entity = board.cell_store.get(position).unwrap();
+        //
+        // FIXME I don't think we want to put em in a cell
+        //
         commands.entity(*entity).with_children(|inside_cell| {
             let goblin_id = inside_cell
                 .spawn(goblin_bundle(position))
--- src/graphics/init_map.rs
+++ src/graphics/init_map.rs
@@ -1,11 +1,27 @@
-use crate::marker_components::*;
-use crate::Board;
+// use crate::marker_components::*;
+use crate::typical::*;
+// use crate::Board;
 use bevy::pbr::OpaqueRendererMethod;
 use bevy::prelude::*;
 
 // slightly larger than 1.0 so the overlap prevents bleed through
 const VOXEL_CUBE_SIZE: f32 = 1.0;
 // const VOXEL_CUBE_MARGIN: f32 = 0.08;
+
+#[derive(Resource, Debug)]
+pub(crate) struct CellCubeEntityMapper(HashMap<Entity, Entity>, HashMap<Entity, Entity>);
+
+// in case we need it for terrain destruction, etc
+impl CellCubeEntityMapper {
+    fn new() -> Self {
+        CellCubeEntityMapper(HashMap::new(), HashMap::new())
+    }
+
+    fn insert(&mut self, cell_entity: Entity, cube_entity: Entity) {
+        self.0.insert(cell_entity, cube_entity);
+        self.1.insert(cube_entity, cell_entity);
+    }
+}
 
 pub(crate) fn spawn_voxel_map(
     mut commands: Commands,
@@ -42,16 +58,19 @@ pub(crate) fn spawn_voxel_map(
         .into(),
     );
 
-    // FIXME
     commands.insert_resource(AmbientLight {
         color: Color::BLACK,
         brightness: 0.0,
     });
 
+    // store a ref to
+    let mut mapper = CellCubeEntityMapper::new();
+
     let bx = 0. - (board.size.x / 2) as f32;
     let by = 0. - (board.size.y / 2) as f32;
 
     let (entity, _marker) = query.single();
+
     commands
         .entity(entity)
         .insert(SpatialBundle {
@@ -59,30 +78,37 @@ pub(crate) fn spawn_voxel_map(
             ..default()
         })
         .with_children(|ch| {
-            for (ivec, entity) in board.cell_store.iter() {
+            for (ivec, cell_entity) in board.cell_store.iter() {
                 let [x, y, z] = ivec.to_array();
 
-                // ?? SHOULD THIS be commands.entity(cell_entity).insert(( ... )) ?n
-
                 // floors
-                ch.spawn((PbrBundle {
-                    mesh: shape.clone(),
-                    material: floor_material.clone(),
-                    transform: Transform::from_xyz(x as f32, y as f32, z as f32 - 1.0),
-                    ..default()
-                },));
+                let cube_entity = ch
+                    .spawn((PbrBundle {
+                        mesh: shape.clone(),
+                        material: floor_material.clone(),
+                        transform: Transform::from_xyz(x as f32, y as f32, z as f32 - 1.0),
+                        ..default()
+                    },))
+                    .id();
+                mapper.insert(*cell_entity, cube_entity);
             }
 
-            for (ivec, entity) in board.wall_store.iter() {
+            for (ivec, cell_entity) in board.wall_store.iter() {
                 let [x, y, z] = ivec.to_array();
 
                 // walls
-                ch.spawn((PbrBundle {
-                    mesh: shape.clone(),
-                    material: floor_material.clone(),
-                    transform: Transform::from_xyz(x as f32, y as f32, z as f32),
-                    ..default()
-                },));
+
+                let cube_entity = ch
+                    .spawn((PbrBundle {
+                        mesh: shape.clone(),
+                        material: floor_material.clone(),
+                        transform: Transform::from_xyz(x as f32, y as f32, z as f32),
+                        ..default()
+                    },))
+                    .id();
+                mapper.insert(*cell_entity, cube_entity);
             }
         });
+
+    commands.insert_resource(mapper);
 }
